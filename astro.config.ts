c2b9fad5df684bda9d34b09fa17f80a06a91283a
jack shelton <me@jackshelton.com>
2024-12-24 20:40:45 -0600

    q astro netlify

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,10 @@
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev()],
+  adapter: netlify(),
 });
